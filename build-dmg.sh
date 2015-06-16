6baad919c0240d9e2e0df8a3e1267851093adb82
Nordlund, Eric <ericn@amazon.com>
2015-06-16 10:10:27 -0700

    Update build script to reduce cruft.

--- build-dmg.sh
+++ build-dmg.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cp -R ~/Library/Audio/Plug-Ins/Components/mda.component ~/github/mda-au-1.0.3/mda.component
-hdiutil create -volname mda-au-1.0.3 -srcfolder ~/github/mda-au-1.0.3 -ov -format UDZO mda-au-1.0.3.dmg
+# Create .dmg image of MDA plug-ins
+
+# Copy plug-in build artifact from ~/Library/Audio/Plug-Ins/Components/
+cp -R ~/Library/Audio/Plug-Ins/Components/mda.component ./mda-au-1.0.3/
+
+# Create .dmg from current folder
+hdiutil create -volname mda-au-1.0.3 -srcfolder ./mda-au-1.0.3 -ov -format UDZO mda-au-1.0.3.dmg
+
+# Remove ACLs from built files
 xattr -d com.apple.FinderInfo mda-au-1.0.3.dmg
 xattr -d com.apple.ResourceFork mda-au-1.0.3.dmg
